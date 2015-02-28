70d3c1cc35fe7e74b3fb205ffd3207d78e858a2e
oferel <oferel1@gmail>
2015-02-28 16:44:40 +0200

    start_1

--- pyma_myweb/settings.py
+++ pyma_myweb/settings.py
@@ -24,6 +24,10 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
+TEMPLATE_DIRS = (
+    'C:/Users/oferel/workspace/pyma_myweb/template',
+)
+
 ALLOWED_HOSTS = []
 
 
@@ -36,6 +40,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -81,3 +86,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+STATIC_ROOT = '/static/'
--- pyma_myweb/urls.py
+++ pyma_myweb/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from main.views import main_page
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,4 +8,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+       (r"^main/$", main_page.as_view()),
 )
