ff7fef92936ade53491adc50685a09777614f68a
oferel <oferel1@gmail>
2015-03-05 11:56:44 +0200

    5.3
    5.3

--- pyma_myweb/settings.py
+++ pyma_myweb/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -85,11 +86,35 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+#STATIC_URL = '/static/'
+
+
+#STATICFILES_DIRS = (
+#   os.path.join(BASE_DIR, "static"),
+#)
+
+#STATIC_ROOT = '/static/'
+
+
+# Parse database configuration from $DATABASE_URL
+
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT = '/static/'
+
+
