c94e9bec18a64eb8628a2549462efd66cf0a2017
oferel <oferel1@gmail>
2015-03-05 21:23:24 +0200

    views
    v

--- main/views.py
+++ main/views.py
@@ -3,4 +3,9 @@ from django.views.generic import TemplateView
 
 class main_page(TemplateView):
     template_name = "main.html"
+    
+    def get_context_data(self, kwargs):
+        context = super(main_page, self).get_context_data(kwargs)
+        return context
+
     #return render (request,"main.html")
