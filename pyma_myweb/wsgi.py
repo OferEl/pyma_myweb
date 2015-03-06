86b769b31048b45778a57e7343e111729c9d8fc7
oferel <oferel1@gmail>
2015-03-06 19:22:53 +0200

    11
    11

--- pyma_myweb/wsgi.py
+++ pyma_myweb/wsgi.py
@@ -14,7 +14,6 @@ from django.core.wsgi import get_wsgi_application
 #application = get_wsgi_application()
 #from whitenoise.django import DjangoWhiteNoise
 from dj_static import Cling
-
 application = Cling(get_wsgi_application())
 
 #application = get_wsgi_application()
