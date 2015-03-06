035603630ebaf97f1729d4b2eeeb38f4ae48270f
oferel <oferel1@gmail>
2015-03-06 15:28:25 +0200

    setting
    setting

--- pyma_myweb/settings.py
+++ pyma_myweb/settings.py
@@ -128,7 +128,11 @@ ALLOWED_HOSTS = ['*']
 
 #BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = '/staticfiles/'
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
+
+STATIC_URL = (
+    os.path.join(BASE_DIR, 'static/img'),
+)
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
