3ddff3c76fc4aafc5e9e6623c725110e080df1bb
oferel <oferel1@gmail>
2015-03-06 19:45:03 +0200

    end
    end

--- pyma_myweb/settings.py
+++ pyma_myweb/settings.py
@@ -127,7 +127,7 @@ ALLOWED_HOSTS = ['*']
 # Static asset configuration
 
 #BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = '/staticfiles/'
 STATIC_URL = '/static/'
 
 #STATIC_URL = (
