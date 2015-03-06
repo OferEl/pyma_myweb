1812f305caf23ec20202b8f332f530343286f06e
oferel <oferel1@gmail>
2015-03-06 15:51:50 +0200

    set
    set

--- pyma_myweb/settings.py
+++ pyma_myweb/settings.py
@@ -131,8 +131,8 @@ STATIC_ROOT = '/staticfiles/'
 STATIC_URL = '/static/'
 
 #STATIC_URL = (
-    os.path.join(BASE_DIR, '/static/'),
-)
+#    os.path.join(BASE_DIR, '/static/'),
+#)
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
